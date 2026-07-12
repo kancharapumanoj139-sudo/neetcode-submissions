ced2d6b0ce1cc95fd07b9216322e4713e46b509d
kancharapumanoj139-sudo <kancharapumanoj139@gmail.com>
2026-07-12 23:09:49 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        if(s.length()==1) return false;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch=='[' || ch=='(' || ch=='{')
+            {
+                st.push(ch);
+            }
+            else
+            {
+                if(st.isEmpty()) return false;
+                char top=st.pop();
+                if(ch==']')
+                {
+                    if(top!='[') return false;
+                }
+                else if(ch==')')
+                {
+                    if(top!='(') return false;
+                }
+                else
+                {
+                    if(top!='{') return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
