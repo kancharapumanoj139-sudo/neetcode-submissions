8e872f3ea8ad29fff74b91329377c324df3ecd3f
kancharapumanoj139-sudo <kancharapumanoj139@gmail.com>
2026-07-12 23:37:15 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for(int a : asteroids) {
+
+            boolean destroyed = false;
+
+            while(!st.isEmpty() &&
+                  st.peek() > 0 &&
+                  a < 0) {
+
+                if(st.peek() < -a) {
+                    st.pop();
+                    continue;
+                }
+
+                else if(st.peek() == -a) {
+                    st.pop();
+                }
+
+                destroyed = true;
+                break;
+            }
+
+            if(!destroyed) {
+                st.push(a);
+            }
+        }
+
+        int[] res = new int[st.size()];
+
+        for(int i = st.size()-1; i >= 0; i--) {
+            res[i] = st.pop();
+        }
+
+        return res;
+    }
+}
