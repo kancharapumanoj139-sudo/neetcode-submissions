8cde7250fbbb2bd00392c9962099acb3771fe5d5
kancharapumanoj139-sudo <kancharapumanoj139@gmail.com>
2026-07-19 10:56:24 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+
+    public void dfs(TreeNode root, int level) {
+
+        if (root == null)
+            return;
+
+        if (level == ans.size())
+            ans.add(root.val);
+
+        dfs(root.right, level + 1);
+        dfs(root.left, level + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        dfs(root, 0);
+        return ans;
+    }
+}
