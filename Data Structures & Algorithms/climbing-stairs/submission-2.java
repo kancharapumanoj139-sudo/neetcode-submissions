d5537716e4830ed17fcbe6d2668c03c90d405c86
kancharapumanoj139-sudo <kancharapumanoj139@gmail.com>
2026-07-22 07:13:50 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int find(int n,int[] dp)
+    {
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int s1=find(n-1,dp);
+        int s2=find(n-2,dp);
+        return dp[n]=s1+s2;
+    }
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
